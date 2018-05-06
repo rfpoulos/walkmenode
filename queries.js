01a485ad8e538bfbc15f6bc0e78b93f399809c3a
rfpoulos <rfpoulos@outlook.com>
2018-05-06 10:26:41 -0400

    creates account fornt for user with hased password, also uses identifier to sign-in and return token

---
+++ queries.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const pg = require('pg-promise')();
+const db = pg(process.env.DB_PATH);
+
+let userByUsername = (identifier) =>
+    db.query(`
+        SELECT * FROM users
+        WHERE username = '${identifier}'
+        OR email = '${identifier}';
+    `);
+
+let createAccountInDb = (email, username, password) =>
+    db.query(`
+        INSERT INTO users (email, username, password)
+        VALUES ('${email}', '${username}', '${password}');
+    `);
+
+module.exports = {
+    userByUsername,
+    createAccountInDb
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const Router = express.Router;
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = new Router();
+
+const { postTokens, createAccount, checkToken } = require('./authorization');
+
+router.post('/signin', postTokens);
+router.post('/createaccount', createAccount);
+// router.use('/', checkToken, api);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+app.use(bodyParser.json());
+app.use(router);
+app.listen(process.env.PORT || 5000);
